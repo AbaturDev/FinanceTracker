207a382c43b931f16dd98439ec30ae8da64d44e9
Abatur <alek@zolnowski.eu>
2025-03-05 19:22:36 +0100

    Add project files

---
+++ src/FinanceTracker.Application/Setup.cs
@@ -0,0 +1,6 @@
+﻿namespace FinanceTracker.Application;
+
+public static class Setup
+{
+
+}
---
+++ src/FinanceTracker.Infrastructure/Context/FinanceTrackerDbContext.cs
@@ -0,0 +1,5 @@
+﻿namespace FinanceTracker.Infrastructure.Context;
+
+internal class FinanceTrackerDbContext
+{
+}
