c5c623102efb01f9e6c93d68457ab9cc96754053
Abatur <alek@zolnowski.eu>
2025-03-29 16:54:02 +0100

    Add missing file

---
+++ src/FinanceTracker.API/Extensions/RegisterEndpointsExtensions.cs
@@ -0,0 +1,15 @@
+using FinanceTracker.API.Endpoints;
+
+namespace FinanceTracker.API.Extensions;
+
+public static class RegisterEndpointsExtensions
+{
+    public static void RegisterEndpoints(this IEndpointRouteBuilder app)
+    {
+        app
+            .MapAccountsApi()
+            .MapExpensesPlannersApi()
+            .MapIncomesApi()
+            .MapUserMonthlyBudgetsApi();
+    }
+}
