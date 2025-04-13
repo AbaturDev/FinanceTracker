a8785df0861aecb4cb4074dcb2ef1e60a3050716
serpenteno <fiacekk@gmail.com>
2025-04-13 23:48:14 +0200

    Seeding fixed

--- src/FinanceTracker.Application/Seeders/UserMonthlyBudgetsSeeder.cs
+++ src/FinanceTracker.Application/Seeders/UserMonthlyBudgetsSeeder.cs
@@ -2,31 +2,31 @@ using Bogus;
 using FinanceTracker.Domain.Entities;
 using FinanceTracker.Infrastructure.Context;
 
-namespace FinanceTracker.Application.Seeders
+namespace FinanceTracker.Application.Seeders;
+
+public class UserMonthlyBudgetsSeeder
 {
-    public class UserMonthlyBudgetsSeeder
+    public static void Seed(FinanceTrackerDbContext dbContext, int budgetsCount)
     {
-        public static void Seed(FinanceTrackerDbContext dbContext, int budgetsCount)
+        if (dbContext.UserMonthlyBudgets.Any())
         {
-            if (dbContext.UserMonthlyBudgets.Any())
-            {
-                return;
-            }
-
-            var sampleUsers = dbContext.Users.ToList();
-
-            var faker = new Faker<UserMonthlyBudget>()
-                .RuleFor(s => s.Date, f => f.Date.SoonDateOnly())
-                .RuleFor(s => s.TotalBudget, f => f.Finance.Amount())
-                .RuleFor(s => s.TotalExpenses, f => f.Finance.Amount())
-                .RuleFor(s => s.CurrencyCode, f => f.PickRandom(sampleUsers).CurrencyCode)
-                .RuleFor(s => s.UserId, f => f.PickRandom(sampleUsers).Id)
-                .RuleFor(s => s.User, f => f.PickRandom(sampleUsers));
-
-            var userMonthlyBudgets = faker.Generate(budgetsCount);
-
-            dbContext.UserMonthlyBudgets.AddRange(userMonthlyBudgets);
-            dbContext.SaveChanges();
+            return;
         }
+
+        var sampleUsers = dbContext.Users.ToList();
+
+        var faker = new Faker<UserMonthlyBudget>()
+            .RuleFor(s => s.Date, f => f.Date.SoonDateOnly())
+            .RuleFor(s => s.TotalBudget, f => f.Finance.Amount())
+            .RuleFor(s => s.TotalExpenses, f => f.Finance.Amount())
+            .RuleFor(s => s.CurrencyCode, f => f.PickRandom(sampleUsers).CurrencyCode)
+            .RuleFor(s => s.UserId, f => f.PickRandom(sampleUsers).Id)
+            .RuleFor(s => s.User, f => f.PickRandom(sampleUsers));
+
+        var userMonthlyBudgets = faker.Generate(budgetsCount);
+
+        dbContext.UserMonthlyBudgets.AddRange(userMonthlyBudgets);
+        dbContext.SaveChanges();
+
     }
 }
--- src/FinanceTracker.Application/Seeders/UsersSeeder.cs
+++ src/FinanceTracker.Application/Seeders/UsersSeeder.cs
@@ -1,32 +1,24 @@
 using Bogus;
 using FinanceTracker.Domain.Entities;
 using FinanceTracker.Infrastructure.Context;
-using Microsoft.AspNetCore.Identity;
 
-namespace FinanceTracker.Application.Seeders
+namespace FinanceTracker.Application.Seeders;
+
+public class UsersSeeder
 {
-    public class UsersSeeder
+    public static void Seed(FinanceTrackerDbContext dbContext, int userCount)
     {
-        public static void Seed(FinanceTrackerDbContext dbContext, int userCount)
+        if (dbContext.Users.Any())
         {
-            if (dbContext.Users.Any())
-            {
-                return;
-            }
-
-            var passwordHasher = new PasswordHasher<User>();
-            var faker = new Faker<User>("pl");
-
-            var users = new List<User>();
-            users.AddRange(faker.Generate(userCount)
-                .Select(u =>
-                {
-                    u.PasswordHash = passwordHasher.HashPassword(u, "haslohaslo");
-                    return u;
-                }));
-
-            dbContext.Users.AddRange(users);
-            dbContext.SaveChanges();
+            return;
         }
+
+        var faker = new Faker<User>("pl")
+            .RuleFor(u => u.CurrencyCode, "PLN");
+        
+        var users = faker.Generate(userCount);
+        
+        dbContext.Users.AddRange(users);
+        dbContext.SaveChanges();
     }
 }
